6619fdc168e183793efef2baa10a829cc99c9aee
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2019-01-01 19:45:55 -0200

    Remove old deprecated calls on fullscreen plugin

--- js/impress.js
+++ js/impress.js
@@ -1529,7 +1529,7 @@
         }, false );
 
         util.triggerEvent( document, "impress:help:add",
-            { command: "b or .", text: "Blackout", row: 100 }
+            { command: "B or .", text: "Blackout", row: 100 }
         );
 
     }, false );
@@ -1686,30 +1686,13 @@
 
     function enterFullscreen() {
         var elem = document.documentElement;
-        if ( !document.fullscreenElement && !document.mozFullScreenElement &&
-             !document.webkitFullscreenElement && !document.msFullscreenElement ) {
-            if ( elem.requestFullscreen ) {
-                elem.requestFullscreen();
-            } else if ( elem.msRequestFullscreen ) {
-                elem.msRequestFullscreen();
-            } else if ( elem.mozRequestFullScreen ) {
-                elem.mozRequestFullScreen();
-            } else if ( elem.webkitRequestFullscreen ) {
-                elem.webkitRequestFullscreen( Element.ALLOW_KEYBOARD_INPUT );
-            }
+        if ( !document.fullscreenElement ) {
+            elem.requestFullscreen();
         }
     }
 
     function exitFullscreen() {
-        if ( document.exitFullscreen ) {
-            document.exitFullscreen();
-        } else if ( document.msExitFullscreen ) {
-            document.msExitFullscreen();
-        } else if ( document.mozCancelFullScreen ) {
-            document.mozCancelFullScreen();
-        } else if ( document.webkitExitFullscreen ) {
-            document.webkitExitFullscreen();
-        }
+        document.exitFullscreen();
     }
 
     // Wait for impress.js to be initialized
--- src/plugins/fullscreen/fullscreen.js
+++ src/plugins/fullscreen/fullscreen.js
@@ -13,30 +13,13 @@
 
     function enterFullscreen() {
         var elem = document.documentElement;
-        if ( !document.fullscreenElement && !document.mozFullScreenElement &&
-             !document.webkitFullscreenElement && !document.msFullscreenElement ) {
-            if ( elem.requestFullscreen ) {
-                elem.requestFullscreen();
-            } else if ( elem.msRequestFullscreen ) {
-                elem.msRequestFullscreen();
-            } else if ( elem.mozRequestFullScreen ) {
-                elem.mozRequestFullScreen();
-            } else if ( elem.webkitRequestFullscreen ) {
-                elem.webkitRequestFullscreen( Element.ALLOW_KEYBOARD_INPUT );
-            }
+        if ( !document.fullscreenElement ) {
+            elem.requestFullscreen();
         }
     }
 
     function exitFullscreen() {
-        if ( document.exitFullscreen ) {
-            document.exitFullscreen();
-        } else if ( document.msExitFullscreen ) {
-            document.msExitFullscreen();
-        } else if ( document.mozCancelFullScreen ) {
-            document.mozCancelFullScreen();
-        } else if ( document.webkitExitFullscreen ) {
-            document.webkitExitFullscreen();
-        }
+        document.exitFullscreen();
     }
 
     // Wait for impress.js to be initialized
